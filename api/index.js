ebf9fe0bc05bca5391f2037452f181f14af90147
Vinita Israni <isranivinita74@gmail.com>
2024-05-10 21:54:01 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-
-app.listen(5000, () =>{
-    console.log("server running");
-})
+mongoose
+  .connect(process.env.DB)
+  .then(() =>{ console.log("database connected")})
+  .catch((error) => console.log(error));
+app.listen(5000, () => {
+  console.log("server running");
+});
