7f51808b7f3b8bace0e8ca720ec2096318e2388b
Vinita Israni <isranivinita74@gmail.com>
2024-05-13 14:58:31 +0530

    Create Profile page UI

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
+import "./profile.css";
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { currentUser } = useSelector((state) => state.user);
 
-export default Profile
+  return (
+    <div className="profile-container">
+      <div className="ovaerlay-profile">
+        <div className="left-container">
+          <div className="profile-right">
+            <div className="profile-bg">
+            <img src={currentUser.avatar} alt="profile" className="profile-img"/>
+            </div>
+            <form className="profile-form">
+              <input
+                type="text"
+                placeholder="Username"
+                className="profile-input"
+                id="username"
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="profile-input"
+                id="email"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="profile-input"
+                id="password"
+              />
+              <button className="profile-btn">Update</button>
+            </form>
+            <div className="profile-actions">
+              <div className="delete-account">Delete Account</div>
+              <div className="log-out-account">Log Out</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
