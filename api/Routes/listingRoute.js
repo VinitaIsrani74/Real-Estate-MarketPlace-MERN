f968c35ff7d9c228779c3f0dc81f0642ea785600
Vinita Israni <isranivinita74@gmail.com>
2024-05-21 21:10:46 +0530

    create search api route

--- api/Routes/listingRoute.js
+++ api/Routes/listingRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createListing, deleteListing, getListing, updateListing } from '../Controllers/listingController.js'
+import { createListing, deleteListing, getListing, getListings, updateListing } from '../Controllers/listingController.js'
 import { validateToken } from '../utils/validateUser.js'
 
 const listingRoute = express.Router()
@@ -9,5 +9,6 @@ listingRoute.post('/create',validateToken, createListing)
 listingRoute.delete('/delete/:id',validateToken, deleteListing)
 listingRoute.post('/update/:id',validateToken, updateListing)
 listingRoute.get('/get/:id',getListing)
+listingRoute.get('/get',getListings)
 
 export default listingRoute
