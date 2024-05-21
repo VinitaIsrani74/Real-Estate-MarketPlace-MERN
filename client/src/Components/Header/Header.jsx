0439a541ddf77e1feda94dd005559e2ab6bcd3cf
Vinita Israni <isranivinita74@gmail.com>
2024-05-21 21:28:38 +0530

    Update header search

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import { IoSearch } from "react-icons/io5";
 import './header.css'
 const Header = () => {
   const {currentUser} = useSelector((state) => state.user)
+  const [searchTerm , setSearchTerm] = useState('')
+  const navigate = useNavigate()
+const handleSubmit = (e) =>{
+  e.preventDefault()
+  const urlParams = new URLSearchParams(window.location.search)
+  urlParams.set('searchTerm', searchTerm)
+  const searchQuery = urlParams.toString()
+navigate(`/search?${searchQuery}`)
+}
+
+useEffect(()=>{
+  const urlParams = new URLSearchParams(location.search)
+  const searchTermFromUrl = urlParams.get('searchTerm')
+  if(searchTermFromUrl){
+    setSearchTerm(searchTermFromUrl)
+  }
+},[location.search])
+
   return (
     <div className='Header'>
         <div className="header-container">
@@ -14,9 +32,12 @@ const Header = () => {
                 <span>Pulse</span>
             </section>
             </Link>
-            <form className='header-form'>
-                <input type="text" placeholder='Search...' className='header-input' id='search'/>
+            <form onSubmit={handleSubmit} className='header-form'>
+                <input type="text" placeholder='Search...' className='header-input' id='search' onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm}/>
+                <button className='search-btn-container'>
                 <IoSearch style={{cursor: "pointer"}}/>
+                </button>
+                
             </form>
             <ul>
             <Link to="/" style={{textDecoration: "none"}}><li className='list1'>Home</li></Link>
